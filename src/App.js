b7e970ff55ffa7460c31a018cfda9e76529f86eb
Ali Ramazon <mr.safarali@gmail.com>
2023-01-28 19:54:12 -0700

    Add Video section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import { Hero } from "./sections/Hero";
 import { TopNav } from "./sections/TopNav";
+import { TrustedBy } from "./sections/TrustedBy";
+import { Advantages } from "./sections/Advantages";
+import { Package } from "./sections/Package";
+import { Video } from "./sections/Video/Video";
+
 const App = () => {
     return (
         <>
             <TopNav />
             <Hero />
+            <TrustedBy />
+            <Advantages />
+            <Package />
+            <Video />
         </>
     );
 };
