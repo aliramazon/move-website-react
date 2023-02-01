88e31086b75cde329a4658c0d6b133d7dbff5dfe
Ali Ramazon <mr.safarali@gmail.com>
2023-02-01 11:07:36 -0700

    Create CTAAction section

---
+++ src/sections/CTAAction/CTAAction.js
@@ -0,0 +1,25 @@
+export const CTAAction = () => {
+    return (
+        <>
+            <section className="cta-section">
+                <div className="row">
+                    <div className="cta-section__text">
+                        <h5>Secure your data in minutes</h5>
+                        <p>Prevent security breaches and save money</p>
+                    </div>
+                    <div className="cta-section__buttons">
+                        <button className="btn btn-lg btn-secondary">
+                            Learn More
+                        </button>
+                        <button className="btn btn-lg btn-primary">
+                            Get Started
+                        </button>
+                    </div>
+                </div>
+            </section>
+            <div className="row">
+                <div className="cta-section__seperator"></div>
+            </div>
+        </>
+    );
+};
